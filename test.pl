d450d5701843a6c0ca77ca8f1f343284a4ba9469
Ben Boeckel <ben.boeckel@kitware.com>
2016-10-27 16:26:20 -0400

    test: name tests

--- test.pl
+++ test.pl
@@ -2,26 +2,29 @@
 
 :- include("depsolve").
 
-expect(Computed, Expected) :-
+expect(Name, Computed, Expected) :-
     subtract(Computed, Expected, []),
     subtract(Expected, Computed, []);
+    print_message(error, Name),
     print_message(error, Computed),
     print_message(error, Expected).
 
-test_deptree(Spec, Expected) :-
+test_deptree(Name, Spec, Expected) :-
     spec_deptree(Spec, Computed),
     !,
-    expect(Computed, Expected).
+    expect(Name, Computed, Expected).
 
 :- initialization(main).
 
 main :-
-    test_deptree(cmake_0,
+    test_deptree("cmake~qt",
+        cmake_0,
         [
             [curl, [7, 49, 1], [], []],
             [zlib, [1, 2, 8], [], []]
         ]),
-    test_deptree(cmake_1,
+    test_deptree("cmake+qt",
+        cmake_1,
         [
             [curl, [7, 49, 1], [], []],
             [qt, [4, 8, 6], [], []],
